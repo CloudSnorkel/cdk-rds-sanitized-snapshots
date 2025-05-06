d7526c53315d813a00e7eea9c2835fafee8bac6d
Amir Szekely <kichik@gmail.com>
2025-05-06 15:46:18 -0400

    fix: Non-cluster snapshot cleanup (#176)
    Fixes #172

--- src/delete-old.lambda.ts
+++ src/delete-old.lambda.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { DeleteDBClusterSnapshotCommand, RDSClient } from '@aws-sdk/client-rds';
+import { DeleteDBClusterSnapshotCommand, DeleteDBSnapshotCommand, RDSClient } from '@aws-sdk/client-rds';
 import { GetResourcesCommand, ResourceGroupsTaggingAPIClient } from '@aws-sdk/client-resource-groups-tagging-api';
 
 const tagging = new ResourceGroupsTaggingAPIClient();
@@ -7,8 +7,8 @@ const rds = new RDSClient();
 
 export interface DeleteOldSnapshotsInput {
   tags: { Key: string; Value: string }[];
+  isCluster: boolean;
   historyLimit: number;
-  resourceType: string;
 }
 
 export async function handler(input: DeleteOldSnapshotsInput) {
@@ -16,7 +16,7 @@ export async function handler(input: DeleteOldSnapshotsInput) {
     TagFilters: input.tags.map(f => {
       return { Key: f.Key, Values: [f.Value] };
     }),
-    ResourceTypeFilters: [input.resourceType],
+    ResourceTypeFilters: [input.isCluster ? 'rds:cluster-snapshot' : 'rds:snapshot'],
   }));
   if (!snapshotsResponse.ResourceTagMappingList) {
     console.error('No snapshots found');
@@ -38,8 +38,14 @@ export async function handler(input: DeleteOldSnapshotsInput) {
 
   for (const snapshot of toDelete) {
     console.log(`Deleting old snapshot: ${snapshot}`);
-    await rds.send(new DeleteDBClusterSnapshotCommand({
-      DBClusterSnapshotIdentifier: snapshot,
-    }));
+    if (input.isCluster) {
+      await rds.send(new DeleteDBClusterSnapshotCommand({
+        DBClusterSnapshotIdentifier: snapshot,
+      }));
+    } else {
+      await rds.send(new DeleteDBSnapshotCommand({
+        DBSnapshotIdentifier: snapshot,
+      }));
+    }
   }
 }
